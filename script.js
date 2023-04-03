35c19d569effac6a09a2d0e4cc6b7c41acccf6c5
NoobHamada <100050458+NoobHamada@users.noreply.github.com>
2023-04-03 11:38:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ let revealProj = document.getElementById("revealProj");
 let images = document.querySelectorAll("img");
 
 // Toggle (Hide / Show) Images Of Past Projects
-revealProj.addEventListener("click", function() {
+revealProj.addEventListener("click", function () {
     for (i = 0; i < images.length; i++) {
         if (images[i].className == "hide") {
             images[i].classList.remove("hide")
@@ -39,18 +39,32 @@ revealProj.addEventListener("click", function() {
             images[i].classList.add("hide")
         }
     }
+
+    // Creating "Click To Open" Text Once Clicked On Show Projects List
+    let info = document.createElement("div");
+    info.innerText = "Click To Open";
+    info.style.color = "red";
+    info.style.position = "fixed";
+    info.style.top = "15%";
+    info.style.left = "15%";
+    document.body.appendChild(info);
+    
+    // Timeout To Remove The (Info) Div
+    setTimeout(function() {
+        info.remove();
+    }, 3000)
 })
 
 // Clicking On Images > Sending To Real Pages
-images[0].onclick = function() {
+images[0].onclick = function () {
     window.open("https://noobhamada.github.io/war3_worthy_maps/", "_blank");
 }
-images[1].onclick = function() {
+images[1].onclick = function () {
     window.open("https://noobhamada.github.io/Calculator/", "_blank");
 }
-images[2].onclick = function() {
+images[2].onclick = function () {
     window.open("https://noobhamada.github.io/Complete-Sequence/", "_blank");
 }
-images[3].onclick = function() {
+images[3].onclick = function () {
     window.open("https://noobhamada.github.io/zombie/", "_blank");
 }
